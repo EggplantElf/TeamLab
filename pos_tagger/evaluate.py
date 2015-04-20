43f4051acbda7de5d60e8f7cdf7b13ef6256ee01
Xiang Yu <eggplant.elf@gmail.com>
2015-04-20 16:34:23 +0200

    add comments

--- pos_tagger/evaluate.py
+++ pos_tagger/evaluate.py
@@ -1,27 +1,32 @@
 from __future__ import division
 import sys
 
+
 def evaluate(gold_file, pred_file):
     gold = []
     pred = []
+    # read the gold and prediction files
     for line in open(gold_file):
         if line.strip(): 
             gold.append(line.split()[1].strip())
-            pred.append("NN")
-    ##for line in open(pred_file):
-    ##    if line.strip():
-    ##        pred.append(line.split()[1].strip())
+    for line in open(pred_file):
+       if line.strip():
+           pred.append(line.split()[1].strip())
     
+    # make sure the two files contains same amount of data
     if len(pred) != len(gold):
         print "The gold file and the prediction file don't correspond"
         exit(0)
+
     pos = set(gold + pred)
     result = {}
+
+    # initialize the counters for true positive, false negative, false positive for each pos tag
     for p in pos:
         result[p] = [0, 0, 0] # tp, fn, fp
-
     correct = 0
 
+    # store the tp, fn, fp 
     for i in range(len(gold)):
         if gold[i] == pred[i]:
             result[gold[i]][0] += 1
@@ -30,6 +35,7 @@ def evaluate(gold_file, pred_file):
             result[gold[i]][1] += 1
             result[pred[i]][2] += 1
 
+    # calculate the precision, recall and f-score for each pos tag
     print 'POS\tPrecision  Recall  F-Score'
     for p in result:
         tp, fn, fp = result[p][0], result[p][1], result[p][2]
@@ -48,6 +54,7 @@ def evaluate(gold_file, pred_file):
 
         print '%s\t  %.2f\t    %.2f    %.2f' % (p, precision, recall, fscore)
         
+    # calculate the macro average precision, recall and f-score 
     print "Average Precision:", sum(result[p][3] for p in result)/len(result)
     print "Average Recall:", sum(result[p][4] for p in result)/len(result)
     print "Average F-Score:", sum(result[p][5] for p in result)/len(result)
